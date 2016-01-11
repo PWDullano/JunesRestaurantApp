09bd7aa88c105326f4cdb4d17c7826e7848a7f31
June Shaw <june.shaw@me.com>
2016-01-11 02:44:45 -0700

    showing image on edit page

--- routes/cuisines.js
+++ routes/cuisines.js
@@ -1,3 +1,12 @@
 module.exports = [
-  'American', 'Italian', 'Mexican', 'Thai', 'Vietnamese'
+  {name:'American',
+  image: '../images/american.png'},
+  {name:'Italian',
+  image: '../images/italian.png'},
+  {name:'Mexican',
+  image: '../images/mexican.png'},
+  {name:'Thai',
+  image: '../images/thai.png'},
+  {name:'Vietnamese',
+  image: '../images/vietnamese.png'}
 ];
