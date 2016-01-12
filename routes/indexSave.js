ec111abe32a5df5d9cf0980d7a5f478558ca5314
June Shaw <june.shaw@me.com>
2016-01-11 21:50:32 -0700

    moved db functions to separate files

--- routes/indexSave.js
+++ routes/indexSave.js
@@ -1,57 +1,73 @@
 var express = require('express');
 var router = express.Router();
-
-var db = require('../src/db.js');
+var db = require('pg');
+var knex = require ('knex')({
+  client:'pg',
+  connection: 'postgres://localhost/venues'
+});
 var states = require("./states.js");
 var cuisines = require("./cuisines.js");
 var ratings = require("./ratings.js");
 
+function restaurants() {
+  return knex('restaurants');
+}
+
+function restaurantDefaults() {
+ return (knex('restaurants').columnInfo().then(function (columns) {
+    var restaurantObject = {};
+    for (var key in columns) {
+      restaurantObject[key] = columns[key].defaultValue;
+    }
+    return restaurantObject;
+  })
+)}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.restaurants().then(function(results) {
+  restaurants().then(function(results) {
     res.render('index', {restaurants:results});
   });
 });
 
 router.get ('/new', function(req, res, next) {
-  db.restaurantDefaults().then(function(results) {
+  restaurantDefaults().then(function(results) {
       res.render('new', {route:req.originalUrl, restaurant:results, states:states, cuisines:cuisines, ratings:ratings});
   });
 });
 
 router.post ('/new', function(req, res, next) {
-  db.insertRestaurant(req.body).then(function(results) {
+  restaurants().insert(req.body).then(function(results) {
     res.redirect('/');
   })
 })
 
 router.get('/:id', function(req, res, next) {
-  console.log('***reqParamId = ', req.params.id);
-  db.getRestaurant(req.params.id).then(function(results) {
+  restaurants().where('id', req.params.id).then(function(results) {
     res.render('show', {restaurant:results[0]});
   })
 })
 
-router.get('/:id/edit', function(req, res, next) {  console.log('get id/edit');
-  db.getRestaurant(req.params.id).then(function(results) {
+router.get('/:id/edit', function(req, res, next) {
+  restaurants().where('id', req.params.id).then(function(results) {
     res.render('edit', {route:req.originalUrl, restaurant:results[0], states:states, cuisines:cuisines, ratings:ratings});
   })
 })
 
 router.get('/:id/delete', function(req, res, next) {
-  db.deleteRestaurant(req.params.id).then(function(results) {
+  restaurants().where('id', req.params.id).del().then(function(results) {
     res.redirect('/');
   })
 })
 
 router.post('/:id/edit', function(req, res, next) {
-  db.updateRestaurant(req.params.id, req.body).then(function() {
+  restaurants().where('id', req.params.id).update(req.body).then(function() {
     res.redirect('/');
   })
 })
 
 router.post('/:id/delete', function(req, res, next) {
-  db.deleteRestaurant(req.params.id).then(function() {
+  restaurants().where('id', req.params.id).del().then(function() {
     res.redirect('/');
   })
 })
---
+++ src/db.js
@@ -0,0 +1,44 @@
+
+// var db = require('pg');
+var knex = require ('knex')({
+  client:'pg',
+  connection: 'postgres://localhost/venues'
+});
+
+function restaurants() {
+  return knex('restaurants');
+}
+
+function restaurantDefaults() {
+ return (knex('restaurants').columnInfo().then(function (columns) {
+    var restaurantObject = {};
+    for (var key in columns) {
+      restaurantObject[key] = columns[key].defaultValue;
+    }
+    return restaurantObject;
+  })
+)}
+
+function insertRestaurant(restaurant) {
+  return(restaurants().insert(restaurant));
+}
+
+function getRestaurant(id) {
+  return(restaurants().where('id', id))
+}
+
+function updateRestaurant(id, restaurant) {
+  return(restaurants().where('id', id).update(restaurant));
+}
+
+function deleteRestaurant(id) {
+  return( restaurants().where('id', id).del());
+}
+
+module.exports =
+{restaurants: restaurants,
+restaurantDefaults: restaurantDefaults,
+insertRestaurant: insertRestaurant,
+getRestaurant: getRestaurant,
+updateRestaurant: updateRestaurant,
+deleteRestaurant: deleteRestaurant}
