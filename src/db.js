24b53f807331d495baadf88dc91a7dc728cec355
Patrick Dullano <PWDullano@gmail.com>
2016-01-18 11:22:38 -0700

    initial deployment

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 // var db = require('pg');
 var knex = require ('knex')({
   client:'pg',
-  connection: 'postgres://localhost/venues'
+  connection: process.env.DATABASE_URL || 'postgres://localhost/venues'
 });
 
 function Restaurants() {
