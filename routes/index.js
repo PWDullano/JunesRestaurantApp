5b32cb5d258aa452e4800ebcfbaca441852e92e4
Patrick Dullano <PWDullano@gmail.com>
2016-01-18 11:54:48 -0700

    ..

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 var db = require('../src/db.js');
 var states = require("./states.js");
@@ -8,7 +9,7 @@ var ratings = require("./ratings.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.restaurants().then(function(results) {
+  db.Restaurants().then(function(results) {
     res.render('index', {restaurants:results});
   });
 });
@@ -25,16 +26,29 @@ router.post ('/new', function(req, res, next) {
   })
 })
 
+router.get ('/admin', function(req, res, next) {
+    db.Restaurants().then(function(restaurants) {
+      res.render('admin/index', {restaurants:restaurants});
+    })
+  });
+
 router.get('/:id', function(req, res, next) {
-  console.log('***reqParamId = ', req.params.id);
-  db.getRestaurant(req.params.id).then(function(results) {
-    res.render('show', {restaurant:results[0]});
+  db.restaurant(req.params.id).then(function(restaurants) {
+    db.restaurantReviews(req.params.id).then(function(reviews) {
+      res.render('show', {restaurant:restaurants[0], reviews:reviews});
+    })
   })
 })
 
-router.get('/:id/edit', function(req, res, next) {  console.log('get id/edit');
-  db.getRestaurant(req.params.id).then(function(results) {
-    res.render('edit', {route:req.originalUrl, restaurant:results[0], states:states, cuisines:cuisines, ratings:ratings});
+router.get('/:id/edit', function(req, res, next) {
+db.restaurant(req.params.id).then(function(results) {
+  res.render('edit', {route:req.originalUrl, restaurant:results[0], states:states, cuisines:cuisines, ratings:ratings});
+})
+})
+
+router.post('/:id/edit', function(req, res, next) {
+  db.updateRestaurant(req.params.id, req.body).then(function() {
+    res.redirect('/');
   })
 })
 
@@ -44,16 +58,108 @@ router.get('/:id/delete', function(req, res, next) {
   })
 })
 
-router.post('/:id/edit', function(req, res, next) {
-  db.updateRestaurant(req.params.id, req.body).then(function() {
-    res.redirect('/');
+router.get('/:id/review/newReview', function(req, res, next) {
+  db.reviewDefaults().then(function(reviews) {
+    res.render('review',
+    {route:req.originalUrl,
+      review:reviews,
+      restaurant_id:req.params.id,
+      ratings: ratings});
   })
 })
 
-router.post('/:id/delete', function(req, res, next) {
-  db.deleteRestaurant(req.params.id).then(function() {
-    res.redirect('/');
+router.post('/:id/review/newReview', function(req, res, next) {
+  db.insertReview(req.body).then(function() {
+    res.redirect('/' + req.params.id);
   })
 })
+
+router.get('/:id/review/:review', function(req, res, next) {
+  db.review(req.params.review).then(function(review) {
+    res.render('review',
+    {route:req.originalUrl,
+    review: review,
+    restaurant_id: req.params.id,
+    ratings: ratings});
+  })
+})
+
+router.post('/:id/review/:review_id', function(req, res, next) {
+  db.updateReview(req.params.review_id, req.body).then(function() {
+    res.redirect('/' + req.params.id);
+  })
+})
+//
+// router.post('/:id/delete', function(req, res, next) {
+//   db.deleteRestaurant(req.params.id).then(function() {
+//     res.redirect('/');
+//   })
+// })
+
+router.get('/:id/admin/edit', function(req, res, next) {
+  db.restaurant(req.params.id).then(function(restaurants) {
+    db.restaurantEmployees(req.params.id).then(function(employees) {
+    res.render('admin/edit',
+    {route: req.originalUrl,
+    restaurant: restaurants[0],
+    employees: employees,
+    states: states,
+    cuisines: cuisines,
+    ratings: ratings});
+    })
+  })
+})
+
+router.post('/:id/admin/edit', function(req, res, next) {
+  console.log('in the id/admin/edit');
+  db.updateRestaurant(req.params.id, req.body).then(function() {
+    res.redirect('/admin');
+  })
+})
+
+router.get('/:id/admin/delete', function(req, res, next) {
+  db.deleteRestaurant(req.params.id).then(function(results) {
+    res.redirect('/admin');
+  })
+})
+
+router.post('/:id/admin/delete', function(req, res, next) {
+  db.deleteEmployee(req.params.id).then(function() {
+    res.redirect('/admin');
+  })
+})
+
+router.get ('/:id/admin/edit/newEmployee', function(req, res, next) {
+  db.employeeDefaults().then(function(results) {
+    res.render('admin/newEmployee', {route:req.originalUrl, employee:results, restaurant_id:req.params.id});
+  });
+});
+
+router.post('/:id/admin/edit/newEmployee', function(req, res, next) {
+  console.log('req.params.id = ', req.params.id);
+  db.insertEmployee(req.body).then(function(results) {
+    var urlString = '/' + req.params.id + '/admin/edit';
+    console.log('urlString = ', urlString);
+    res.redirect(urlString);
+  })
+})
+
+router.get ('/:id/admin/edit/employee/:employee_id', function(req, res, next) {
+  console.log('employee id= *** ', req.params.employee_id);
+  db.employee(req.params.employee_id).then(function(employees) {
+    res.render('admin/employee', {route:req.originalUrl, employee:employees[0], restaurant_id:req.params.id});
+
+    //  restaurant_id:req.params.id});
+  });
+});
+
+router.post('/:id/admin/edit/employee/:employee_id', function(req, res, next) {
+  db.updateEmployee(req.params.employee_id, req.body).then(function(results) {
+    console.log('req.params.id = ', req.params.id);
+    console.log('employee id= *** ', req.params.employee_id);
+    res.redirect('/' + req.params.id + '/admin/edit');
+  })
+})
+
 
 module.exports = router;
--- src/db.js
+++ src/db.js
@@ -5,40 +5,129 @@ var knex = require ('knex')({
   connection: 'postgres://localhost/venues' || process.env.DATABASE_URL
 });
 
-function restaurants() {
+function Restaurants() {
   return knex('restaurants');
 }
 
+function Employees() {
+  return knex('employees');
+}
+
+function Reviews() {
+  return knex('reviews');
+}
+
+function restaurantEmployees(id) {
+  return(Restaurants().rightJoin('employees', 'employees.restaurant_id', 'restaurants.id').where('restaurant_id', id));
+};
+
 function restaurantDefaults() {
- return (knex('restaurants').columnInfo().then(function (columns) {
+ return (Restaurants().columnInfo().then(function (columns) {
     var restaurantObject = {};
     for (var key in columns) {
       restaurantObject[key] = columns[key].defaultValue;
     }
-    return restaurantObject;
+    return(restaurantObject);
+  })
+)}
+
+function employeeDefaults() {
+ return (Employees().columnInfo().then(function (columns) {
+    var Object = {};
+    for (var key in columns) {
+      Object[key] = columns[key].defaultValue;
+    }
+    return(Object);
+  })
+)}
+
+function reviewDefaults() {
+  return (Reviews().columnInfo().then(function (columns) {
+    var reviewObject = {};
+    for (var key in columns) {
+      reviewObject[key] = columns[key].defaultValue;
+    }
+    return(reviewObject);
   })
 )}
 
 function insertRestaurant(restaurant) {
-  return(restaurants().insert(restaurant));
+  return(Restaurants().insert(restaurant));
 }
 
-function getRestaurant(id) {
-  return(restaurants().where('id', id))
+function restaurant(id) {
+  console.log("in restaurant retrieve: ", id);
+  return(Restaurants().where('id', id))
 }
 
 function updateRestaurant(id, restaurant) {
-  return(restaurants().where('id', id).update(restaurant));
+  return(Restaurants().where('id', id).update(restaurant));
 }
 
 function deleteRestaurant(id) {
-  return( restaurants().where('id', id).del());
+  console.log('in delete restaurant');
+  return(Employees().leftJoin('restaurants', 'restaurants.id', 's.restaurant_id').where('restaurant_id', id).del().then (function () {
+    console.log('about to delete restaurant of ', id)
+     restaurant(id).del();
+  })
+)}
+
+function insertEmployee(employee) {
+  return(Employees().insert(employee));
+}
+
+function employee(id) {
+  return(Employees().where('id', id))
+}
+
+function updateEmployee(id, employee) {
+  return(Employees().where('id', id).update(employee));
+}
+
+function deleteEmployee(id) {
+  return(Employees().where('id', id).del());
+}
+
+function insertReview(review) {
+  console.log('insertReview with ', review);
+  console.log();
+  return(Reviews().insert(review));
+}
+
+function review(id) {
+  return(Reviews().where('id', id))
+}
+
+function updateReview(id, review) {
+  return(Reviews().where('id', id).update(review));
+}
+
+function deleteReview(id) {
+  return(Reviews().where('id', id).del());
+}
+
+function restaurantReviews(id) {
+  return (Restaurants().join('reviews', 'restaurants.id', 'reviews.restaurant_id').select('reviews.content', 'reviews.date', 'reviews.rating'));
 }
 
 module.exports =
-{restaurants: restaurants,
+{Restaurants: Restaurants,
 restaurantDefaults: restaurantDefaults,
+employeeDefaults: employeeDefaults,
+reviewDefaults: reviewDefaults,
+restaurantEmployees: restaurantEmployees,
+restaurantReviews: restaurantReviews,
 insertRestaurant: insertRestaurant,
-getRestaurant: getRestaurant,
+restaurant: restaurant,
 updateRestaurant: updateRestaurant,
-deleteRestaurant: deleteRestaurant}
+deleteRestaurant: deleteRestaurant,
+insertEmployee: insertEmployee,
+employee: employee,
+updateEmployee: updateEmployee,
+deleteEmployee: deleteEmployee,
+insertReview: insertReview,
+review: review,
+updateReview: updateReview,
+deleteReview: deleteReview,
+restaurantReviews: restaurantReviews
+}
